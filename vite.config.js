4cd33b5cf2f5608d4417d843121aec1eea2ee2dc
alice zak. <47920627+notsousual@users.noreply.github.com>
2025-06-09 15:36:18 +0200

    netlify plugin added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import netlify from '@netlify/vite-plugin';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), netlify(), svgr()],
 });
